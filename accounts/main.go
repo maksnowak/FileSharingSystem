50df74872f4936e740a4b68d561a62863b9b3e73
Duszke333 <duszke333@gmail.com>
2024-11-21 20:35:42 +0100

    Added coverage test

--- accounts/main.go
+++ accounts/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func addOne(x int) int {
+	return x + 1
+}
+
 func main() {
 	fmt.Println("Hello from `accounts` microservice!")
+	fmt.Println("Adding 1 to 3...")
+	fmt.Println(addOne(3))
 }
