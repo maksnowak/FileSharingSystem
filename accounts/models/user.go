20cadcfc5e64a325d17829c6bc74453de1737469
Duszke333 <duszke333@gmail.com>
2024-12-11 16:00:15 +0100

    Fix ID type

--- accounts/models/user.go
+++ accounts/models/user.go
@@ -1,18 +1,21 @@
 package models
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"time"
+)
 
 // # User
 //
 // A structure representing MongoDB User structure
 type User struct {
-	ID           string    `bson:"_id,omitempty" json:"id,omitempty"`
-	Username     string    `bson:"username" json:"username"`
-	Email        string    `bson:"email" json:"email"`
-	PasswordHash string    `bson:"passwordHash" json:"passwordHash"`
-	PasswordSalt string    `bson:"passwordSalt" json:"passwordSalt"`
-	CreatedAt    time.Time `bson:"createdAt" json:"createdAt"`
-	Role         string    `bson:"role" json:"role"`
-	OwnedFiles   []string  `bson:"ownedFiles" json:"ownedFiles"`
-	SharedFiles  []string  `bson:"sharedFiles" json:"sharedFiles"`
+	ID           bson.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username     string        `bson:"username" json:"username"`
+	Email        string        `bson:"email" json:"email"`
+	PasswordHash string        `bson:"passwordHash" json:"passwordHash"`
+	PasswordSalt string        `bson:"passwordSalt" json:"passwordSalt"`
+	CreatedAt    time.Time     `bson:"createdAt" json:"createdAt"`
+	Role         string        `bson:"role" json:"role"`
+	OwnedFiles   []string      `bson:"ownedFiles" json:"ownedFiles"`
+	SharedFiles  []string      `bson:"sharedFiles" json:"sharedFiles"`
 }
