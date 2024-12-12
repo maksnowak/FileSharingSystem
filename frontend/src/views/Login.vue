da897344cb182acf8263dcd94e05dcd196e24b32
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-12-12 15:44:16 +0100

    Update login page look

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,7 +3,17 @@ import LoginForm from '../components/LoginForm.vue'
 </script>
 
 <template>
-    <main>
+    <main class="login-page">
         <LoginForm />
     </main>
 </template>
+
+<style scoped>
+.login-page {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
