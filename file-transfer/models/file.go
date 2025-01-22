824a3d0a44c8b546598a6b9fb61b61618a151dec
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-22 14:41:03 +0100

    added get files by user

--- file-transfer/models/file.go
+++ file-transfer/models/file.go
@@ -1,15 +1,15 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type File struct {
-	FileID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	FileName  string             `json:"file_name,omitempty" bson:"fileName,omitempty"`
-	UserID    string             `json:"user_id,omitempty" bson:"userID,omitempty"`
-	Tags      []string           `json:"tags,omitempty" bson:"tags,omitempty"`
-	Path      string             `json:"path,omitempty" bson:"path,omitempty"`
-	BlobURL   string             `json:"blob_url,omitempty" bson:"blobURL,omitempty"`
-	HasAccess []string           `json:"has_access,omitempty" bson:"hasAccess,omitempty"` // List of user IDs
+	FileID    bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	FileName  string        `json:"file_name,omitempty" bson:"fileName,omitempty"`
+	UserID    string        `json:"user_id,omitempty" bson:"userID,omitempty"`
+	Tags      []string      `json:"tags,omitempty" bson:"tags,omitempty"`
+	Path      string        `json:"path,omitempty" bson:"path,omitempty"`
+	BlobURL   string        `json:"blob_url,omitempty" bson:"blobURL,omitempty"`
+	HasAccess []string      `json:"has_access,omitempty" bson:"hasAccess,omitempty"` // List of user IDs
 }
